f0d128a96d9c11e5449f1caa93d7022474a27b0b
Akash <150268192+akash2904r@users.noreply.github.com>
2024-07-31 19:26:43 +0530

    Add files via upload

---
+++ src/utils/fileSystem.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+import path from "path";
+
+const DIR = '../src/public/temp';
+
+export const checkFileExistence = async (imgId: string) => {
+    let foundImage;
+
+    const files = await fs.promises.readdir(DIR);
+
+    for (const file of files) {
+        const fileExists = file.includes(imgId);
+
+        if (fileExists) {
+            foundImage = path.join(DIR, file);
+            break;
+        }
+    }
+
+    return foundImage;
+};
